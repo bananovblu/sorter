73b529381ab1f84993f7b0b156f38fcd0da477ab
Yauheni Baranau <barapcmax@gmail.com>
2018-09-12 16:45:19 +0300

    q

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.sortArray = [];
+    this.compare_func = function (left, right) {
+      return left - right;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.sortArray.push(element);
   }
 
   at(index) {
-    // your implementation
+    if(index >= this.sortArray.length){
+      return -1;
+    }
+
+    return this.sortArray[index];
   }
 
   get length() {
-    // your implementation
+    return this.sortArray.length;
   }
 
   toArray() {
-    // your implementation
+    return this.sortArray;
   }
 
   sort(indices) {
-    // your implementation
+      indices.sort((a, b) => a - b);
+      let tArray = [];
+
+      for(let i = 0; i < indices.length; i++){
+        tArray.push(this.sortArray[indices[i]]);
+      }
+
+      tArray.sort(this.compare_func);
+
+      for(let i = 0; i < indices.length; i++){
+          this.sortArray[indices[i]] = tArray[i];
+      }
+
+      return this.sortArray;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compare_func = compareFunction;
   }
 }
 
